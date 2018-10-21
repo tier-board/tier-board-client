8d8cce68f4db7bd4bd45f9a29183c2b114056466
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-10-21 12:44:58 -0700

    added registerSW.js, modified package.json

---
+++ build/precache-manifest.4b0a1ac6a1a837e7bab8045e7309d433.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a292bd6602ba1c5add69",
+    "url": "/tier-board-client/static/js/runtime~main.a292bd66.js"
+  },
+  {
+    "revision": "6b2fb190a29938cffa9b",
+    "url": "/tier-board-client/static/js/main.6b2fb190.chunk.js"
+  },
+  {
+    "revision": "8fc23ac99a4fa3a4e09e",
+    "url": "/tier-board-client/static/js/1.8fc23ac9.chunk.js"
+  },
+  {
+    "revision": "eb7ecd5de7e492c43c4ba8fb5d361952",
+    "url": "/tier-board-client/index.html"
+  }
+];
