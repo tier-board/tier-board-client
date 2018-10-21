ce188740679a39c83ff76cbcf0b6b3ff45f47bd5
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-10-20 18:37:43 -0700

    linter fixes

--- src/lib/reducer.js
+++ src/lib/reducer.js
@@ -12,95 +12,79 @@ export const SAFETY_DELETE = 'safety/DELETE';
 // Reducer
 let initialState = [];
 export default (state = initialState, action) => {
-   let {type, payload} = action;
-    switch (type) {
-        case SAFETY_ADD:
-        localStorage.setItem('state', 'state');
-          return [...state, payload];
-        case SAFETY_FETCH:
-          return [...state, payload];
-        case SAFETY_ASYNC_ADD:
-          return [...state, ...payload];
-        case SAFETY_UPDATE:  
-          return state.map(safety => safety.id === payload.id ? payload : safety);
-        case SAFETY_DELETE: 
-          return state.filter(safety => safety.id !== payload.id);
-        default: return state;
-    }
-}
+  let {type, payload} = action;
+  switch (type) {
+  case SAFETY_ADD:
+    localStorage.setItem('state', 'state');
+    return [...state, payload];
+  case SAFETY_FETCH:
+    return [...state, payload];
+  case SAFETY_ASYNC_ADD:
+    return [...state, ...payload];
+  case SAFETY_UPDATE:  
+    return state.map(safety => safety.id === payload.id ? payload : safety);
+  case SAFETY_DELETE: 
+    return state.filter(safety => safety.id !== payload.id);
+  default: return state;
+  }
+};
 
 // Action Creators
 export const safetyAdd = (safety) => {
-    console.log('addReducer', safety);
-    safety.id = uuid();
-    return dispatch => {
-        superagent.post(url, safety)
-          .then(res => {
-            dispatch({type: SAFETY_ADD, payload: res.body})
-          });
-      };
+  console.log('addReducer', safety);
+  safety.id = uuid();
+  return dispatch => {
+    superagent.post(url, safety)
+      .then(res => {
+        dispatch({type: SAFETY_ADD, payload: res.body});
+      });
+  };
 };
 
 export const safetyAsyncAdd = (safety) => {
-    console.log('inside action creator', safety)
-    return {
-      type: SAFETY_ASYNC_ADD,
-      payload: safety,
-    };
+  console.log('inside action creator', safety);
+  return {
+    type: SAFETY_ASYNC_ADD,
+    payload: safety,
   };
+};
 export const safetyFetch = () => {
-    return dispatch => {
-      fetch(url)
-        .then(function(res) { 
-          return res.json();
-        })
-        .then((safety) => {
-            console.log('fetch data: ', safety);
-          dispatch(safetyAsyncAdd(safety));
-        });
-    }
-}
-// export const safetyFetch = () => {
-//     return dispatch => {
-//         superagent.get(url)
-//         //.then((res) => res.json())
-//         .then(res => {
-//             console.log('res', res);
-//         //     return res.json();
-//         })
-//             .then((res) => {
-//             dispatch({
-//                 type: SAFETY_FETCH,
-//                 payload: res,
-//             });
-//         });
-//     }
-    
-// };
+  return dispatch => {
+    fetch(url)
+      .then(function(res) { 
+        return res.json();
+      })
+      .then((safety) => {
+        console.log('fetch data: ', safety);
+        dispatch(safetyAsyncAdd(safety));
+      });
+  };
+};
+
 export const safetyUpdate = (safety) => {
-    return dispatch => {
-        superagent.put(`${url}/${safety._id}`)
-        .send(safety)
-        .then(() => {
-            dispatch({ 
-                type: SAFETY_UPDATE,
-                payload: safety,
-            })
-        })
-    }
+  return dispatch => {
+    superagent.put(`${url}/${safety._id}`)
+      .send(safety)
+      .then(() => {
+        dispatch({ 
+          type: SAFETY_UPDATE,
+          payload: safety,
+        });
+      });
+  };
    
 };
 export const safetyDelete = (safety) => {
-    return dispatch => {
-      superagent.delete(`${url}/${safety._id}`)
+  return dispatch => {
+    superagent.delete(`${url}/${safety._id}`)
       .then(res => {
         return res.text;
       })
       .then(() => {
         dispatch(
-         {type: SAFETY_DELETE,
-          payload: safety,
-          })
+          {type: SAFETY_DELETE,
+            payload: safety,
+          });
       });
-    }
-}
+  };
+};
