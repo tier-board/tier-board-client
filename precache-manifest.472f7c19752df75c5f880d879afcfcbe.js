8b26429b6b36be78d5261698173af4eec9f6ca87
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-11-03 17:31:25 -0700

    Updates

--- precache-manifest.472f7c19752df75c5f880d879afcfcbe.js
+++ precache-manifest.472f7c19752df75c5f880d879afcfcbe.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/tier-board-client/static/js/runtime~main.a292bd66.js"
   },
   {
-    "revision": "9edfa89ee8f531c64bcb",
-    "url": "/tier-board-client/static/js/main.9edfa89e.chunk.js"
+    "revision": "c787d433706dee9b8b84",
+    "url": "/tier-board-client/static/js/main.c787d433.chunk.js"
   },
   {
     "revision": "1198dad3c1663f7951cc",
     "url": "/tier-board-client/static/js/1.1198dad3.chunk.js"
   },
   {
-    "revision": "58b0d1084151f7600bb9e5c413ec9d08",
+    "revision": "68146a87dd61ff0e54ed508bd9dd537f",
     "url": "/tier-board-client/index.html"
   }
 ];
